6f750205a235b868baf341b92ca9d7147028c89c
Mieszko Cichon <svider.mc@gmail.com>
2017-01-17 21:07:47 +0100

    Create new files, and several classes (...) 2x operator overloading, sample inheritance and etc.

---
+++ ResourceManager/body/interherence/ArrayOfObjects.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+#include "Employee.h"
+
+class ArrayOfObjectsMainClass
+{
+protected:
+	static inline void sendMessage(void) noexcept
+	{
+		std::cout << "\n\tTo ja - piekna tablica obiektow\n";
+		return;
+	}
+};
+
+template<typename T>
+class ArrayOfObjects : public ArrayOfObjectsMainClass
+{
+public:
+	void readMessage()
+	{
+		sendMessage();
+	}
+private:
+	T value{ "" };
+public:
+	std::string value1() const
+	{
+		return value;
+	}
+public:
+	void set_value(const std::string& value)
+	{
+		this->value = value;
+	}
+public:
+	__declspec(property(get = value1, put = set_value)) std::string Value;
+};
